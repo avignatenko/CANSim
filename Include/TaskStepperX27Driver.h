813252d5516f99fbf456b69946e12357aff31096
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2022-03-21 14:06:07 +0300

    Updated x27 stepper driver task

--- Include/TaskStepperX27Driver.h
+++ Include/TaskStepperX27Driver.h
@@ -7,17 +7,26 @@ class AccelStepper;
 class TaskStepperX27Driver: private Task
 {
 public:
-    TaskStepperX27Driver(Scheduler& sh, byte step, byte dir, byte reset);
+    TaskStepperX27Driver(Scheduler& sh, byte step, byte dir, byte reset, bool limit = true);
 
     void start();
 
+    void stop();
+    void resetPosition(int16_t position);
     void setPosition(int16_t position);
     int16_t position() const;
+    int16_t targetPosition() const;
     uint16_t totalSteps() const;
+
+    void setMaxSpeed(int maxSpeed);
+    void setMaxAcceleration(int maxAcceleration);
 
 private:
     bool Callback() override;
 
 private:
     AccelStepper* motor_;
+    bool limit_;
+    int maxSpeed_;
+    int maxAcceleration_;
 };
--- Src/TaskStepperX27Driver.cpp
+++ Src/TaskStepperX27Driver.cpp
@@ -10,42 +10,66 @@ bool TaskStepperX27Driver::Callback()
     return true;
 }
 
-TaskStepperX27Driver::TaskStepperX27Driver(Scheduler& sh, byte step, byte dir, byte reset)
-    : Task(TASK_IMMEDIATE, TASK_FOREVER, &sh, false)
+TaskStepperX27Driver::TaskStepperX27Driver(Scheduler& sh, byte step, byte dir, byte reset, bool limit)
+    : Task(TASK_IMMEDIATE, TASK_FOREVER, &sh, false), limit_(limit)
 {
     // standard X25.168 range 315 degrees at 1/3 degree steps, but 1/12 degree with driver
     motor_ = new AccelStepper(AccelStepper::DRIVER, step, dir);
 
     motor_->setPinsInverted(true);  // invert direction
     motor_->setEnablePin(reset);
+
+    // per datasheet speed limit 600 deg / second = 600 * 12 steps / second
+    constexpr int kMaxSpeed = 600 * 12 * 0.7;  // steps per second
+    constexpr int kAcceleration = 9000;
+    maxSpeed_ = kMaxSpeed;
+    maxAcceleration_ = kAcceleration;
 }
 
 void TaskStepperX27Driver::start()
 {
     motor_->enableOutputs();
 
-    // per datasheet speed limit 600 deg / second = 600 * 12 steps / second
-    constexpr int kMaxSpeed = 600 * 12 * 0.7;  // steps per second
-    constexpr int kAcceleration = 9000;
+    motor_->setCurrentPosition(0);
 
-    // set calibration speed & accelration
-    motor_->setMaxSpeed(kMaxSpeed / 3);
-    motor_->setAcceleration(kAcceleration / 2);
+    if (limit_)
+    {
+        // set calibration speed & accelration
+        motor_->setMaxSpeed(maxSpeed_ / 3);
+        motor_->setAcceleration(maxAcceleration_ / 2);
 
-    motor_->setCurrentPosition(kTotalSteps - 1);
-    motor_->moveTo(0);
-    motor_->runToPosition();
+        motor_->setCurrentPosition(kTotalSteps - 1);
+        motor_->moveTo(0);
+        motor_->runToPosition();
+    }
 
     // set normal speed & acceleration
-    motor_->setMaxSpeed(kMaxSpeed);
-    motor_->setAcceleration(kAcceleration);
+    motor_->setMaxSpeed(maxSpeed_);
+    motor_->setAcceleration(maxAcceleration_);
 
     enable();
 }
 
+void TaskStepperX27Driver::setMaxSpeed(int maxSpeed)
+{
+    maxSpeed_ = maxSpeed;
+    motor_->setMaxSpeed(maxSpeed_);
+}
+
+void TaskStepperX27Driver::setMaxAcceleration(int maxAcceleration)
+{
+    maxAcceleration_ = maxAcceleration;
+    motor_->setAcceleration(maxAcceleration_);
+}
+
+void TaskStepperX27Driver::resetPosition(int16_t position)
+{
+    motor_->setCurrentPosition(position);
+}
+
 void TaskStepperX27Driver::setPosition(int16_t steps)
 {
-    motor_->moveTo(constrain(steps, 0, kTotalSteps - 1));
+    motor_->moveTo(limit_ ? constrain(steps, 0, kTotalSteps - 1) : steps);
 }
 
 int16_t TaskStepperX27Driver::position() const
@@ -53,7 +77,17 @@ int16_t TaskStepperX27Driver::position() const
     return motor_->currentPosition();
 }
 
+int16_t TaskStepperX27Driver::targetPosition() const
+{
+    return motor_->targetPosition();
+}
+
 uint16_t TaskStepperX27Driver::totalSteps() const
 {
     return kTotalSteps;
+}
+
+void TaskStepperX27Driver::stop()
+{
+    motor_->stop();
 }
